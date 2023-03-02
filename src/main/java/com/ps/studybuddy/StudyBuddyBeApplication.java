29b763a05fd0a4b5b78dbad831884b63a913ea98
tudorceltare <tudor.pitic@gmail.com>
2023-03-02 12:53:37 +0200

    First commit

---
+++ src/main/java/com/ps/studybuddy/StudyBuddyBeApplication.java
@@ -0,0 +1,13 @@
+package com.ps.studybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBuddyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ps/studybuddy/StudyBuddyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.studybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
